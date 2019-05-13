5c6d1a1a3d557bf02093b434e3d0595abfbe0f98
Teddy2M6 <47573307+Teddy2M6@users.noreply.github.com>
2019-05-13 16:45:12 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------------------------
 --
--- Created by: Aden Rao
+-- Created by: Teddy Sannan
 -- Created on: April 16, 2019
 --
 -- This program lets you click a d-pad either right, left, up or down and lets you move the character and the game has physics involved so there is some gravity and it makes the characters movements more natural.
